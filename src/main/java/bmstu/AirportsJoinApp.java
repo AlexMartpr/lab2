0ea3dc0b36a08d65d4e9f58fd49d261cd318f564
AlexMartPr <alexmartpr@gmail.com>
2021-10-23 01:06:52 +0300

    Scripted auto-commit on change (2021-10-23 01:06:52) by gitwatch.sh

--- src/main/java/bmstu/AirportsJoinApp.java
+++ src/main/java/bmstu/AirportsJoinApp.java
@@ -3,17 +3,29 @@ package bmstu;
 import java.io.IOException;
 
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.Job;
 
 public class AirportsJoinApp {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
         Job job = Job.getInstance();
         job.setJarByClass(AirportsJoinApp.class);
         job.setJobName("JoinAirports");
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightMap.class);
-        
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportMap.class);
+
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        job.setPartitionerClass(AirportPartitioner.class);
+        job.setGroupingComparatorClass(AirportComparator.class);
+        job.setReducerClass(JoinAirportReducer.class);
+        job.setMapOutputKeyClass(AirportWritableComparable.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        job.setNumReduceTasks(2);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
