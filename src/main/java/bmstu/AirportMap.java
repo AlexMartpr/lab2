552f2852efa4f93b5dd4dd8b860a981ad9535896
AlexMartPr <alexmartpr@gmail.com>
2021-10-23 02:28:11 +0300

    Scripted auto-commit on change (2021-10-23 02:28:11) by gitwatch.sh

--- src/main/java/bmstu/AirportMap.java
+++ src/main/java/bmstu/AirportMap.java
@@ -11,7 +11,7 @@ public class AirportMap extends Mapper<LongWritable, Text, AirportWritableCompar
     protected void map(LongWritable key, Text value,
             Mapper<LongWritable, Text, AirportWritableComparable, Text>.Context context)
             throws IOException, InterruptedException {
-                String[] values = value.toString().split("\",\"");
+                String[] values = value.toString().split(",");
                 if(key.get() > 0) {
                     String clearStringAirportID = values[0].replaceAll("\"", "");
                     int airportID = Integer.parseInt(clearStringAirportID);
