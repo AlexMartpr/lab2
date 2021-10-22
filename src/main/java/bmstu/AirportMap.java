154e784f18e05279ac784a2a0c37b89922391e0a
AlexMartPr <alexmartpr@gmail.com>
2021-10-23 00:30:24 +0300

    Scripted auto-commit on change (2021-10-23 00:30:24) by gitwatch.sh

--- src/main/java/bmstu/AirportMap.java
+++ src/main/java/bmstu/AirportMap.java
@@ -6,7 +6,17 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class AirportMap extends Mapper<LongWritable, Text, Text, AirportWritableComparable>{
-    
-
+public class AirportMap extends Mapper<LongWritable, Text, AirportWritableComparable, Text>{
+    @Override
+    protected void map(LongWritable key, Text value,
+            Mapper<LongWritable, Text, AirportWritableComparable, Text>.Context context)
+            throws IOException, InterruptedException {
+                String[] airport = value.toString().split("\",\"");
+                if(key.get()!=0) {
+                    String clearStringAirportID = airport[0].replaceAll("\"", "");
+                    int airportID = Integer.parseInt(clearStringAirportID);
+                    String airportName = airport[1].replaceAll("\"", "");
+                    context.write(new AirportWritableComparable(airportID, 0), new Text(airportName));
+                }
+    }
 }
