43c941967fa87308074a45db0f8e35193bc96c52
AlexMartPr <alexmartpr@gmail.com>
2021-10-22 22:00:27 +0300

    Scripted auto-commit on change (2021-10-22 22:00:27) by gitwatch.sh

---
+++ src/main/java/bmstu/AirportWritableComparable.java
@@ -0,0 +1,52 @@
+package bmstu;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import org.apache.hadoop.io.WritableComparable;
+
+public class AirportWritableComparable implements WritableComparable<AirportWritableComparable>{
+
+    private int airportID;
+    private int dataID;
+
+    public AirportWritableComparable(int aID, int dID) {
+        this.airportID = aID;
+        this.dataID = dID;
+    }
+
+    public int getAirportID() {
+        return this.airportID;
+    }
+
+    public int getDataID() {
+        return this.dataID;
+    }
+
+    @Override
+    public void write(DataOutput out) throws IOException {
+        out.writeInt(airportID);
+        out.writeInt(dataID);
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException {
+        airportID = in.readInt();
+        dataID = in.readInt();
+        
+    }
+
+    @Override
+    public int compareTo(AirportWritableComparable o) {
+        int compareAirportID = airportID > o.airportID ? 1 : -1;
+        if (airportID == o.airportID) compareAirportID = 0;
+        if (compareAirportID != 0) {
+            return compareAirportID;
+        } else {
+            return Integer.compare(dataID, o.dataID);
+        }
+    }
+    
+    
+}
