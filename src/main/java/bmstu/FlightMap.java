973daac69a9a47053516dc4ba19644a4012a3867
AlexMartPr <alexmartpr@gmail.com>
2021-10-23 00:20:22 +0300

    Scripted auto-commit on change (2021-10-23 00:20:22) by gitwatch.sh

---
+++ src/main/java/bmstu/FlightMap.java
@@ -0,0 +1,25 @@
+package bmstu;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class FlightMap extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
+    
+    private final int positionAirportID = 14;
+    private final int positionAirportDelay = 18;
+
+    @Override
+    protected void map(LongWritable key, Text value,
+            Mapper<LongWritable, Text, AirportWritableComparable, Text>.Context context)
+            throws IOException, InterruptedException {
+        String[] cleatValues = value.toString().split(",");
+        if (key.get() != 0) {
+            String airportStringID = cleatValues[14];
+            int airportID = Integer.parseInt(airportStringID);
+            String airportDelay = 
+        }
+    }
+}
